e225775f451c36026db2448172a993affb9f8489
Fezile Mpotulo <fsmpotulo@gmail.com>
2025-04-28 21:17:58 +0200

    addition to repo and age variable

---
+++ src/main/java/org/example/Variables.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(myAge);
+    }
+}
